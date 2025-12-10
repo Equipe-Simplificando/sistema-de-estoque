bf17bfd8f6cc90af838d485023ba556b3fcd5c05
YuriCardosoSMD <yuri.cardososmd@gmail.com>
2025-12-10 11:09:45 -0300

    Adição de feedback de carregamento na página de Login

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,15 +4,20 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     const usuario = document.getElementById("usuario").value.trim();
     const senha = document.getElementById("senha").value.trim();
     const msg = document.getElementById("mensagem");
+    const loader = document.getElementById("loading");
 
-    // LIMPA MENSAGEM
+    // LIMPA MENSAGEM E GARANTE QUE O LOADER ESTÁ OCULTO
     msg.textContent = "";
+    loader.style.display = "none";
 
     // LOGIN CORRETO
     if (usuario === "admin" && senha === "admin") {
 
         msg.style.color = "green";
         msg.textContent = "Login realizado com sucesso!";
+
+        // MOSTRA O LOADER
+        loader.style.display = "block";
 
         // AGUARDA 1 SEGUNDO E REDIRECIONA
         setTimeout(() => {
@@ -23,5 +28,8 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
         // LOGIN INCORRETO
         msg.style.color = "red";
         msg.textContent = "Usuário ou senha incorretos!";
+        
+        // Garante loader oculto no erro
+        loader.style.display = "none";
     }
 });
